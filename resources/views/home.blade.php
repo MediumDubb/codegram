57bc7974503629a903105066378fb53afe8185ac
Noah Whittington <noahwhittington@Noahs-MacBook-Air.local>
2021-09-06 21:41:05 -0400

    eddited home view, added new profile schema/model/controller, changed up some of the default models and controllers, added 1:1 relationship between Profile and User models

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 p-5">
+            <img src="" alt="profile img">
+        </div>
+        <div class="col-md-8 d-flex flex-column p-5">
+            <div><h1>{{ $user->username}}</h1></div>
+            <div class="d-flex">
+                <div class="pr-3">
+                    posts: <strong>173</strong>
+                </div>
+                <div class="pr-3">
+                    followers: <strong> 2M</strong>
+                </div>
+                <div class="pr-3">
+                    following:<strong>212</strong>
+                </div>
+            </div>
+            <h2>{{$user->profile->title}}</h2>
+            <p>{{ $user->profile->description }}</p>
+            <div>
+                <a href="httpd://www.google.com">Google.com</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
